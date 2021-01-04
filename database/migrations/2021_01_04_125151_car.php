60ddf0260a85531ce0e3dc9845735ba985c32137
AlFlores10 <alberto.flores.1990@gmail.com>
2021-01-04 14:18:55 +0100

    Feature finish Migrations

---
+++ database/migrations/2021_01_04_125151_car.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Car extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50);
+            $table->string('maxSpeed', 20);
+            $table->string('tank', 10);
+            $table->string('photo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cars');
+    }
+}
